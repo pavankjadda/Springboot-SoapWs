d61b8494cd88e5a9bbb0a0b7d39c354713646580
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-08 17:45:20 -0400

    First Commit

---
+++ src/main/java/com/springdemo/soapws/SoapwsApplication.java
@@ -0,0 +1,15 @@
+package com.springdemo.soapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SoapwsApplication.class, args);
+    }
+
+}
